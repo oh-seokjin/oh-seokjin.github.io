98fd0f531b059da1a687b0e1a904cc97ad2f5c23
oh-seokjin <5hseokjin@gmail.com>
2026-02-23 00:51:15 +0900

    [EDIT] navigation bar, title, commennt feature

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "test",
+        "excerpt":"Daily  데일리  ","categories": ["Daily"],
+        "tags": ["ail tag"],
+        "url": "/daily/2026/02/22/test.html",
+        "teaser": null
+      },{
+        "title": "test",
+        "excerpt":"Test Heading  테스트 본문  ","categories": ["Review"],
+        "tags": ["test tag"],
+        "url": "/review/2026/02/22/test.html",
+        "teaser": null
+      }]
