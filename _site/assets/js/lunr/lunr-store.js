618f03a4b2049283d80fec787a0ea81dab2cf9ff
oh-seokjin <5hseokjin@gmail.com>
2026-02-23 10:30:29 +0900

    [ADD] date on posts

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "NLP",
+        "excerpt":"Natural Language Processing  테스트  ","categories": ["NLP"],
+        "tags": ["NLP","TEST"],
+        "url": "/nlp/2026/02/22/test.html",
+        "teaser": null
+      },{
         "title": "test",
         "excerpt":"Daily  데일리  ","categories": ["Daily"],
         "tags": ["ail tag"],
